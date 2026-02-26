331f74677dcb5e0990d0bed6e59546ce7c15cd73
Peter-Phi-Tran <ptran.tech@outlook.com>
2026-02-25 20:17:03 -0600

    fix base path for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import icon from "astro-icon";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  site: "https://Peter-Phi-Tran.github.io",
+  base: "/website",
   integrations: [icon()],
 
   vite: {
